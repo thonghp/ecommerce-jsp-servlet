4a0372c103544b882d59a8f2288dfddb5ace26d5
thonghp <thonghoangpham123@gmail.com>
2023-01-18 21:25:07 +0700

    add hash md5 for password

---
+++ src/main/java/com/hpt/ecommercejspservlet/entity/HashGenerator.java
@@ -0,0 +1,37 @@
+package com.hpt.ecommercejspservlet.entity;
+
+import com.hpt.ecommercejspservlet.entity.exception.HashGenerationException;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class HashGenerator {
+    private HashGenerator() {}
+
+    public static String generateMD5(String message) throws HashGenerationException {
+        return hashString(message, "MD5");
+    }
+
+    private static String hashString(String message, String algorithm) throws HashGenerationException {
+        try {
+            MessageDigest md = MessageDigest.getInstance(algorithm);
+            byte[] hash = md.digest(message.getBytes(StandardCharsets.UTF_8));
+            StringBuilder hexString = new StringBuilder();
+
+            for (int i = 0; i < hash.length; i++) {
+                String hex = Integer.toHexString(0xff & hash[i]);
+                if (hex.length() == 1) {
+                    hexString.append('0');
+                }
+
+                hexString.append(hex);
+            }
+
+            return hexString.toString();
+        } catch (NoSuchAlgorithmException e) {
+            throw new HashGenerationException("Could not generate hash from String", e);
+        }
+    }
+}
+
---
+++ src/main/java/com/hpt/ecommercejspservlet/entity/exception/HashGenerationException.java
@@ -0,0 +1,6 @@
+package com.hpt.ecommercejspservlet.entity.exception;
+public class HashGenerationException extends RuntimeException {
+    public HashGenerationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
